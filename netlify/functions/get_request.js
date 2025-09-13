a5c5a36f99d96b9990223c5a139ca800b8f77471
git-thinh <thinhifis@gmail.com>
2025-09-13 22:21:36 +0000

    Remove old version be netlify folder

--- netlify/functions/get_request.js
+++
@@ -1,9 +0,0 @@
-
-import { initDB, resOPTIONS, resJson, getRequest } from './api.js'
-
-export const handler = async (event, context, callback) => {
-    if (event.httpMethod === "OPTIONS") return resOPTIONS
-    const db = await initDB(event)
-    const r = await getRequest(db, event)
-    return resJson(r)
-}
